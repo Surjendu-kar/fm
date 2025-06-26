2e8be3cc0a6fb5432faf9dda9de47024b3dd9bc5
Surjendu kar <rahulgotp9988@gmai.com>
2025-06-26 15:18:35 +0530

    Add keyframes animation and styling for new box component

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,20 @@ function App() {
           repeatType: "reverse",
         }}
       />
+      {/* keyframes animation */}
+      <motion.div
+        className="box-3"
+        animate={{
+          // path animation
+          x: [0, 1200, 1200, 0, 0],
+          y: [0, 0, 300, 300, 0],
+          rotate: [0, 10, 0, -10, 0],
+        }}
+        transition={{
+          duration: 3,
+          delay: 1,
+        }}
+      />
     </div>
   );
 }
